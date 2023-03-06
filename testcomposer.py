f16879c16adf47fd8bae6720d6e583320f06287e
Chinthagunta Raghu <51625476+craghuvarun@users.noreply.github.com>
2023-03-06 07:23:17 +0530

    Update testcomposer.py

--- testcomposer.py
+++ testcomposer.py
@@ -1,34 +1,27 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.google.cloud.operators.gcs import GoogleCloudStorage
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 
-# Define the start date for the DAG
-start_date = datetime(year=2023, month=3, day=5)
-
-# Define the default arguments for the DAG
 default_args = {
-    'owner': 'your_name',
+    'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': start_date,
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': datetime(2023, 3, 6),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-# Create the DAG
 dag = DAG(
     'my_dag',
     default_args=default_args,
-    description='My DAG',
+    description='A simple DAG',
     schedule_interval=timedelta(days=1),
 )
 
-# Create the storage task
-storage_task = GoogleCloudStorage(
-    task_id='write_file',
-    bucket='testingpurpose',
-    object='file.txt',
-    
-    dag=dag,
-)
+def print_hello():
+    return 'Hello world!'
+
+with dag:
+    t1 = PythonOperator(
+        task_id='print_hello',
+        python_callable=print_hello,
+    )
